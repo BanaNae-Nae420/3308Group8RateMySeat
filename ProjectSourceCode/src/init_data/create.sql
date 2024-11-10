a4d4899742f1c7deab134ac78f0a47b06d8c3078
Carson Mattei <62523084+carsomatte@users.noreply.github.com>
2024-11-09 19:29:22 -0700

    start of lab 11 and /addReview endpoint

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,10 +1,83 @@
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    password VARCHAR(60) NOT NULL,
-    email VARCHAR(60) NOT NULL
+    username VARCHAR(60) PRIMARY KEY,
+    password VARCHAR(60) NOT NULL
 );
 
+-- reviews
+DROP TABLE IF EXISTS reviews CASCADE;
+CREATE TABLE IF NOT EXISTS reviews (
+  review_id SERIAL PRIMARY KEY NOT NULL,
+  review VARCHAR(200),
+  rating DECIMAL NOT NULL
+);
+
+-- image for reviews
+DROP TABLE IF EXISTS images CASCADE;
+CREATE TABLE IF NOT EXISTS images (
+  image_id SERIAL PRIMARY KEY NOT NULL,
+  image_url VARCHAR(300) NOT NULL,
+  image_caption VARCHAR(200)
+);
+
+-- stadium seat
+DROP TABLE IF EXISTS seats CASCADE;
+CREATE TABLE IF NOT EXISTS seats (
+    seat_id SERIAL PRIMARY KEY NOT NULL,
+    section INT,
+    row INT,
+    seat_number INT
+    -- FOREIGN KEY (stadium_id) REFERENCES stadiums(stadium_id)
+);
+
+-- stadium event
+DROP TABLE IF EXISTS events CASCADE;
+CREATE TABLE IF NOT EXISTS events (
+    event_id SERIAL PRIMARY KEY NOT NULL, 
+    event_name VARCHAR(60),
+    event_date DATE
+    -- FOREIGN KEY (stadium_id) REFERENCES stadiums(stadium_id)
+);
+
+-- review to user
+DROP TABLE IF EXISTS reviews_to_users CASCADE;
+CREATE TABLE reviews_to_users (
+  username VARCHAR(60) NOT NULL,
+  review_id INT NOT NULL,
+  FOREIGN KEY (username) REFERENCES users (username) ON DELETE CASCADE,
+  FOREIGN KEY (review_id) REFERENCES reviews (review_id) ON DELETE CASCADE
+);
+
+-- image to review
+DROP TABLE IF EXISTS reviews_to_images CASCADE;
+CREATE TABLE reviews_to_images (
+  image_id INT NOT NULL,
+  review_id INT NOT NULL,
+  FOREIGN KEY (image_id) REFERENCES images (image_id) ON DELETE CASCADE,
+  FOREIGN KEY (review_id) REFERENCES reviews (review_id) ON DELETE CASCADE
+);
+
+-- seat to review
+DROP TABLE IF EXISTS reviews_to_seats CASCADE;
+CREATE TABLE reviews_to_seats (
+    review_id INT NOT NULL,
+    seat_id INT NOT NULL,
+    FOREIGN KEY (review_id) REFERENCES reviews (review_id) ON DELETE CASCADE,
+    FOREIGN KEY (seat_id) REFERENCES seats (seat_id) ON DELETE CASCADE
+);
+
+-- event to review
+DROP TABLE IF EXISTS reviews_to_events CASCADE;
+CREATE TABLE reviews_to_events (
+    review_id INT NOT NULL,
+    event_id INT NOT NULL,
+    FOREIGN KEY (review_id) REFERENCES reviews (review_id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events (event_id) ON DELETE CASCADE
+);
+
+
+
+
+/*
 CREATE TABLE stadiums ( -- possibly not need
     stadium_id SERIAL PRIMARY KEY, 
     stadium_name VARCHAR(60)
@@ -41,4 +114,4 @@ CREATE TABLE reviews (
     rating INT NOT NULL,
     comment TEXT
 );
-
+*/
---
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO EVENTS (event_name, event_date)
+VALUES ('zach bryan', '2024-11-15');
